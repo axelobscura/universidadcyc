3d38fc24b2826de6784d12c2ec427861221446da
Axel <axosar@gmail.com>
2023-01-11 19:20:15 -0600

    mui

--- pages/index.js
+++ pages/index.js
@@ -1,12 +1,63 @@
 
+import * as React from 'react';
 import Layout from './components/Layout'
 import styles from '../styles/Home.module.css'
+import Box from '@mui/material/Box';
+import TextField from '@mui/material/TextField';
+import FilledInput from '@mui/material/FilledInput';
+import InputLabel from '@mui/material/InputLabel';
+import InputAdornment from '@mui/material/InputAdornment';
+import FormControl from '@mui/material/FormControl';
+import Visibility from '@mui/icons-material/Visibility';
+import VisibilityOff from '@mui/icons-material/VisibilityOff';
+import IconButton from '@mui/material/IconButton';
+import Button from '@mui/material/Button';
+import SendIcon from '@mui/icons-material/Send';
 
 export default function Home() {
+  const [showPassword, setShowPassword] = React.useState(false);
+
+  const handleClickShowPassword = () => setShowPassword((show) => !show);
+
+  const handleMouseDownPassword = (event) => {
+    event.preventDefault();
+  };
   return (
     <Layout>
       <h1 className={styles.title}>Universidad Del Cemento y del Concreto</h1>
-      <p>Instituto Mexicano del Cemento y del Concreto A.C.</p> 
+      <p>Instituto Mexicano del Cemento y del Concreto A.C.</p>
+      <Box
+        component="form"
+        sx={{
+          '& > :not(style)': { m: 1, width: '25ch' },
+        }}
+        noValidate
+        autoComplete="off"
+      >
+        <TextField id="filled-basic" label="Usuario" variant="filled" />
+        <FormControl sx={{ m: 1, width: '25ch' }} variant="filled">
+          <InputLabel htmlFor="filled-adornment-password">Contraseña</InputLabel>
+          <FilledInput
+            id="filled-adornment-password"
+            type={showPassword ? 'text' : 'password'}
+            endAdornment={
+              <InputAdornment position="end">
+                <IconButton
+                  aria-label="toggle password visibility"
+                  onClick={handleClickShowPassword}
+                  onMouseDown={handleMouseDownPassword}
+                  edge="end"
+                >
+                  {showPassword ? <VisibilityOff /> : <Visibility />}
+                </IconButton>
+              </InputAdornment>
+            }
+          />
+        </FormControl>
+        <Button variant="contained" endIcon={<SendIcon />} style={{'height':'55px'}}>
+          INGRESAR
+        </Button>
+      </Box>
     </Layout> 
   )
 }
