0a0b0bdd0601a1ce6934abc6ac1817204d7e7bf4
Axel <axosar@gmail.com>
2023-01-16 20:02:18 -0600

    libro

--- pages/categoria/[id].js
+++ pages/categoria/[id].js
@@ -7,11 +7,3 @@ export default function Categoria() {
         </Layout>
     );
 }
-
-export async function getStaticPaths() {
-    // Return a list of possible value for id
-}
-
-export async function getStaticProps({ params }) {
-    // Fetch necessary data for the blog post using params.id
-}
