0afa71921602b7b19c0dbab132b2fe46a6f6afdd
Axel <axosar@gmail.com>
2023-01-17 18:12:37 -0600

    ff

--- pages/components/Menu.js
+++ pages/components/Menu.js
@@ -1,11 +1,10 @@
 import * as React from 'react';
+import Link from 'next/link';
 import Box from '@mui/material/Box';
 import List from '@mui/material/List';
 import ListItem from '@mui/material/ListItem';
 import ListItemButton from '@mui/material/ListItemButton';
 import ListItemIcon from '@mui/material/ListItemIcon';
-import ListItemText from '@mui/material/ListItemText';
-import Divider from '@mui/material/Divider';
 import AddIcon from '@mui/icons-material/Add';
 import AutoStoriesIcon from '@mui/icons-material/AutoStories';
 import ImportContactsIcon from '@mui/icons-material/ImportContacts';
@@ -13,6 +12,7 @@ import AssignmentIndIcon from '@mui/icons-material/AssignmentInd';
 import AutoAwesomeMotionIcon from '@mui/icons-material/AutoAwesomeMotion';
 import AttributionIcon from '@mui/icons-material/Attribution';
 import BallotIcon from '@mui/icons-material/Ballot';
+import HomeIcon from '@mui/icons-material/Home';
 
 export default function Menu({ menuIten, categorias }) {
 
@@ -26,6 +26,15 @@ export default function Menu({ menuIten, categorias }) {
     <Box sx={{ width: '100%', maxWidth: 360, bgcolor: 'transparent', color: 'white', fontSize: '2rem' }}>
       <nav aria-label="main mailbox folders">
         <List>
+          <Link href='/panel'>
+            <ListItem disablePadding>
+              <ListItemButton className='icono-menu'>
+                <ListItemIcon className='icono-menu'>
+                  <HomeIcon sx={{ color: '#fff' }} />
+                </ListItemIcon>
+              </ListItemButton>
+            </ListItem>
+          </Link>
           {categorias.map(val => (
             <ListItem disablePadding key={val.id} onClick={() => menuIten(val.titulo)}>
               <ListItemButton className='icono-menu'>
